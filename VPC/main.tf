b49e845582dce9778bbf7ad1f5d42e47e8e8cf25
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-11-29 18:43:59 -0800

    fixing bugs

--- VPC/main.tf
+++ VPC/main.tf
@@ -9,21 +9,53 @@ resource "azurerm_network_security_group" "terraform" {
   name                = var.sec_group
   location            = azurerm_resource_group.terraform.location
   resource_group_name = azurerm_resource_group.terraform.name
+
+   custom_rules = [
+    {
+      name                   = "http"
+      priority               = 201
+      direction              = "Inbound"
+      access                 = "Allow"
+      protocol               = "tcp"
+      source_port_range      = "*"
+      destination_port_range = "80"
+      source_address_prefix  = var.vnet_address_space
+      description            = "Open porst 80 for HTTP traffic"
+    },
+    {
+      name                    = "https"
+      priority                = 200
+      direction               = "Inbound"
+      access                  = "Allow"
+      protocol                = "tcp"
+      source_port_range       = "*"
+      destination_port_range  = "443"
+      source_address_prefixes = var.vnet_address_space
+      description             = "Open port 443 for HTTPS Traffic"
+    },
+  ]
+
+  tags = {
+    environment = "dev"
+  }
+
+  depends_on = [azurerm_resource_group.terraform]
 }
 
-resource "azurerm_network_security_rule" "terraform" {
-  name                        = "HTTP"
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.terraform.name
-  network_security_group_name = azurerm_network_security_group.terraform.name
-}
+#THIS IS COMMENTED BECAUSE IT ALLOWS ONLY ONE RULE AT A TIME, EXAMPLE ABOVE LOOKS LIKE A MORE SUITABLE METHOD
+# resource "azurerm_network_security_rule" "terraform" {
+#   name                        = "HTTP"
+#   priority                    = 100
+#   direction                   = "INBOUND"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = azurerm_resource_group.terraform.name
+#   network_security_group_name = azurerm_network_security_group.terraform.name
+# }
 
 
 #Creating virtual netwok to the current resource group
@@ -53,7 +85,7 @@ resource "azurerm_virtual_network" "terraform" {
   }
 
   tags = {
-    environment = "VNET"
+    environment = "dev"
   }
 }
 

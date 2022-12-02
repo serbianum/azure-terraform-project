85d5d70c0eba57216cf9e1708459c2fd222dfe49
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-01 18:37:26 -0800

    deleted Linux scale set code

--- VPC/main.tf
+++ VPC/main.tf
@@ -45,42 +45,4 @@ resource "azurerm_virtual_network" "terraform" {
    
  }
   
- resource "azurerm_linux_virtual_machine_scale_set" "terraform" {
-  name                = "terraform-vmss"
-  resource_group_name = azurerm_resource_group.terraform.name
-  location            = azurerm_resource_group.terraform.location
-  sku                 = "Standard_F2"
-  instances           = 1
-  admin_username      = "adminuser"
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDU7DWDdaSk8VxH29TJhmF1j2gd+h0USTboymQdRBUsc7xVgjZumWznghwxs3AmajcsZuigCP0Jkh+qJ545hWBtv2Y7uhynJ/mbpmNjyP/gXKPYW8igkyrMOkOsyBQBfqr2di5IOTojc0xHlLcUcalosc4RwL4K8wEQvcd+aD1Stqa0uG6JV/vAYDGb8XY6BS10MrRb+uPmZDM5J3AwiYkDsiI1qbFHfuMibUST4zAkCU5hy+/2ZHN4mPXREKBL4U7rER+iN4hG9RTaIrjJYkm3meByZepFaZEEayx39A831EPpKMep8amWOWWjAZO7ipieDu+5tOecy2+d6XgKCjvH9qY/WtphWTW1sf3cIidA69/I0JqG/XemvpZBY+liicSuVy6mvWUlY2HbHt+7mKxzHf3IzYNNdtvkfFvj6bNxHvH/d75HDw+awiNSRgKvti5vYp30OdkKy4LX/zbuAXqivSvIE4jkdL4xWwAL8yjzfGCwBD4OtI8w8TV4VwLshFc= mihai@cc-6faa9033-6857b67664-z87lb"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
-  }
-
-  network_interface {
-    name    = "terraform"
-    primary = true
-
-    ip_configuration {
-      name      = "terraform"
-      primary   = true
-      subnet_id = azurerm_subnet.terraform[each.key].id
-    }
-  }
-}
  
-
-

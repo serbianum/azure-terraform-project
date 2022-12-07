ba8ce6be8614ffd6d80b220b11d8314c290d8ef3
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-06 20:05:19 -0800

    making it 'simple'

--- VPC/main.tf
+++ VPC/main.tf
@@ -100,7 +100,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   name                = "vmss"
   resource_group_name = azurerm_resource_group.terraform.name
   location            = azurerm_resource_group.terraform.location
-  sku                 = "Standard_F2"
+  sku                 = "Standard_D2S_v3"
   instances           = 1
   admin_username      = "adminuser"
   custom_data = filebase64("customdata.tpl")

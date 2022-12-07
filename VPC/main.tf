3bd728ad8dd9e26b9d363bafcd99ec996b08d397
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-06 18:39:44 -0800

    making it 'simple'

--- VPC/main.tf
+++ VPC/main.tf
@@ -129,7 +129,7 @@ resource "azurerm_lb_backend_address_pool" "bpepool" {
 }
 
 resource "azurerm_lb_probe" "wordpress" {
-  resource_group_name = azurerm_resource_group.terraform.name
+  #resource_group_name = azurerm_resource_group.terraform.name
   loadbalancer_id     = azurerm_lb.wp_lb.id
   name                = "ssh-running-probe"
   port                = 80

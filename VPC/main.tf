04644a342af8260f97167e7df9af61ef711d47ea
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-06 18:44:00 -0800

    making it 'simple'

--- VPC/main.tf
+++ VPC/main.tf
@@ -142,7 +142,7 @@ resource "azurerm_lb_rule" "lbnatrule" {
   protocol                       = "Tcp"
   frontend_port                  = 80
   backend_port                   = 80
-  backend_address_pool_ids        = azurerm_lb_backend_address_pool.bpepool.id
+  backend_address_pool_ids        = [azurerm_lb_backend_address_pool.bpepool.id]
   frontend_ip_configuration_name = "PublicIPAddress"
   probe_id                       = azurerm_lb_probe.wordpress.id
 }

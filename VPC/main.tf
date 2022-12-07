dc3f5ad8a6e5611d38e1efb6646146f6b90f0974
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-06 19:22:31 -0800

    making it 'simple'

--- VPC/main.tf
+++ VPC/main.tf
@@ -9,22 +9,46 @@ resource "azurerm_network_security_group" "nsg" {
   name                = var.sec_group
   location            = azurerm_resource_group.terraform.location
   resource_group_name = azurerm_resource_group.terraform.name
+
+  security_rule {
+    name                        = "HTTP"
+    priority                    = 100
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "80"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
+
+  security_rule {
+    name                        = "HTTP"
+    priority                    = 100
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "22"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
 }
 
 #Open port 80 not sure this is how we supposed to do it
-resource "azurerm_network_security_rule" "terraform" {
-  name                        = "HTTP"
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "80"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.terraform.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
+# resource "azurerm_network_security_rule" "terraform" {
+#   name                        = "HTTP"
+#   priority                    = 100
+#   direction                   = "Outbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_range      = "80"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = azurerm_resource_group.terraform.name
+#   network_security_group_name = azurerm_network_security_group.nsg.name
+# }
 
 
 #Creating virtual netwok to the current resource group

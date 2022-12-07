ff7a96eb26f184cc773df3b8f702ec91e6af6550
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-06 18:30:57 -0800

    making it 'simple'

--- VPC/main.tf
+++ VPC/main.tf
@@ -47,6 +47,21 @@ resource "azurerm_virtual_network" "vnet" {
    
  }
 
+resource "azurerm_public_ip" "public_ip" {
+  name                = "wordpress-public-ip"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.wordpress.name
+  allocation_method   = "Static"
+  domain_name_label   = random_string.random_string.result
+}
+
+resource "random_string" "random_string" {
+  length  = 6
+  special = false
+  upper   = false
+  number  = false
+}
+
 #Assigning a security group to all existing subnets
 resource "azurerm_subnet_network_security_group_association" "nsg-assoc" {
   for_each                  = var.subnets
@@ -96,3 +111,38 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   }
 }
  
+resource "azurerm_lb" "wp_lb" {
+  name                = "wordpress-lb"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.terraform.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  loadbalancer_id = azurerm_lb.wp_lb.id
+  name            = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_probe" "wordpress" {
+  resource_group_name = azurerm_resource_group.terraform.name
+  loadbalancer_id     = azurerm_lb.wp_lb.id
+  name                = "ssh-running-probe"
+  port                = 80
+}
+
+resource "azurerm_lb_rule" "lbnatrule" {
+  resource_group_name            = azurerm_resource_group.terraform.name
+  loadbalancer_id                = azurerm_lb.wp_lb.id
+  name                           = "http"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = azurerm_lb_probe.wordpress.id
+}

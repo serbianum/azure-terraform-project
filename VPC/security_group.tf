b35ca851f9b92e2c86d7d40ecdb2692471ef2bfb
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-07 21:21:57 -0800

    config

--- VPC/security_group.tf
+++ VPC/security_group.tf
@@ -33,3 +33,6 @@ resource "azurerm_subnet_network_security_group_association" "nsg-assoc" {
   for_each                  = var.subnets
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+

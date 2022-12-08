52dbafc86f89925ac01d5f486e135c637474fc92
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-07 21:26:27 -0800

    config

--- VPC/resource_group.tf
+++ VPC/resource_group.tf
@@ -4,9 +4,9 @@ resource "azurerm_resource_group" "terraform" {
   location = var.region
 }
 
-# resource "random_string" "random_s" {
-#   length  = 6
-#   special = false
-#   upper   = false
-#   number  = false
-# }
+resource "random_string" "random_s" {
+  length  = 6
+  special = false
+  upper   = false
+  number  = false
+}
--- VPC/security_group.tf
+++ VPC/security_group.tf
@@ -30,7 +30,6 @@ resource "azurerm_network_security_group" "nsg" {
 
 #Assigning a security group to all existing subnets
 resource "azurerm_subnet_network_security_group_association" "nsg-assoc" {
-  for_each                  = var.subnets
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }

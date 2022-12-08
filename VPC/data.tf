1cc574b0918ab952dd94e52360b3d3a59a0700e9
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-07 21:13:10 -0800

    added output

--- VPC/data.tf
+++ VPC/data.tf
@@ -1,3 +1,12 @@
-data "nic"{
-    value = azurerm_network_interface.ss_vm_nic.ip
+data "azurerm_public_ip" "ss_ip" {
+  name                = "name_of_public_ip"
+  resource_group_name = "name_of_resource_group"
+}
+
+output "domain_name_label" {
+  value = "${data.azurerm_public_ip.ss_ip.domain_name_label}"
+}
+
+output "public_ip_address" {
+  value = "${data.azurerm_public_ip.ss_ip.ip_address}"
 }
--- VPC/output.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip"{
-    value = module.azurerm_network_interface.nic.ip
-}

bed14eb603df11b616774ff51b96e216c4ab37d1
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-07 21:54:19 -0800

    config

--- VPC/scale_set.tf
+++ VPC/scale_set.tf
@@ -61,7 +61,6 @@ resource "azurerm_lb_backend_address_pool" "bpepool" {
 }
 
 resource "azurerm_lb_nat_pool" "lbnatpool" {
-  resource_group_name            = azurerm_resource_group.terraform.name
   name                           = "ssh"
   loadbalancer_id                = azurerm_lb.example.id
   protocol                       = "Tcp"

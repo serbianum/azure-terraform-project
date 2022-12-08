6f5b4c0d7c5893abd7c9adffb262982e9b103bda
Mihai Serbianu <mihai.serbianu95@gmail.com>
2022-12-07 21:57:48 -0800

    config

--- VPC/data.tf
+++ VPC/data.tf
@@ -1,12 +1,12 @@
-data "azurerm_public_ip" "ss_ip" {
-  name                = "name_of_public_ip"
-  resource_group_name = "name_of_resource_group"
-}
+# data "azurerm_public_ip" "ss_ip" {
+#   name                = "name_of_public_ip"
+#   resource_group_name = "name_of_resource_group"
+# }
 
-output "domain_name_label" {
-  value = "${data.azurerm_public_ip.ss_ip.domain_name_label}"
-}
+# output "domain_name_label" {
+#   value = "${data.azurerm_public_ip.ss_ip.domain_name_label}"
+# }
 
-output "public_ip_address" {
-  value = "${data.azurerm_public_ip.ss_ip.ip_address}"
-}
+# output "public_ip_address" {
+#   value = "${data.azurerm_public_ip.ss_ip.ip_address}"
+# }
